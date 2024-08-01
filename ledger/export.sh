dddf68e52bd18f1a533fa57539cab2adc746d1cd
Clément Salaün <salaun.clement@gmail.com>
2024-08-01 11:17:11 +0200

    fix: export

--- ledger/export.sh
+++ ledger/export.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Export the ledger log
- curl -X POST http://localhost:3068/v2/xyz/logs/export -o /ledger.export.log
+ curl -X POST http://localhost:8080/v2/default/logs/export -o /ledger.export.log
